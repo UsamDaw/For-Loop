b6fa265a01b23d2cd51354032b3f9ff90fd99895
UsamDaw <usama2000dawod@gmail.com>
2024-02-06 11:09:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+/*
+Task 1
+for (let i = 0; i < 51; i++) {
+    console.log(i)
+    
+}
+*/
+
+
+/*
+Task 2
+for (let i = 2; i <=50; i+=2) {
+    console.log(i)
+
+}
+*/
+
+
+/*
+Task 3
+for (let i = 0; i < 100; i++) {
+    if (i%3 == 0 && i %5 == 0) {
+        console.log(i)
+    }
+
+}
+*/
+
+
+/*
+Task 4
+for (let i = 0; i < 100; i++) {
+    if (i%3 == 0 || i %5 == 0) {
+        console.log(i)
+    }
+
+}
+*/
+
+
+/*
+Task 5
+document.getElementById("a").textContent = `Røyken VGS er bra.`;
+document.getElementById("b").textContent = `Røyken VGS er bra.`;
+document.getElementById("c").textContent = `Røyken VGS er bra.`;
+document.getElementById("d").textContent = `Røyken VGS er bra.`;
+document.getElementById("e").textContent = `Røyken VGS er bra.`;
+document.getElementById("f").textContent = `Røyken VGS er bra.`;
+*/
+
+
+/*
+Task 6
+Source: https://www.youtube.com/watch?v=ooe2KBJjMiA
+for(i=1; i<=7; i++)
+{
+    for(k=1; k<=16; k++)
+{
+    if(i==1 || i==7 || k==11 || k==1)
+{
+    document.write("*");
+}else
+document.write(" &nbsp;&nbsp; ");
+}
+document.write("<br>");
+}
+*/
+
+/*
+Task 7
+document.write(" &nbsp; &nbsp; ");
+document.write("N = 1", "<br>");
+for(i=1; i<=1; i++)
+{
+    for(j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+document.write(" &nbsp; &nbsp; ")
+document.write("N = 2", "<br>")
+for(i=1; i<=2; i++)
+{
+    for(j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+document.write(" &nbsp; &nbsp; ")
+document.write("N = 3", "<br>")
+for(i=1; i<=3; i++)
+{
+    for(j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+document.write(" &nbsp; &nbsp; ")
+document.write("N = 4", "<br>")
+for(i=1; i<=4; i++)
+{
+    for(j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+document.write("<br>");
+
+
+document.write(" &nbsp; &nbsp; ")
+document.write("N = 5", "<br>")
+for(i=1; i<=5; i++)
+{
+    for(j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
+    }
+    for(k=1; k<=((2*i)-1); k++)
+    {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+*/
